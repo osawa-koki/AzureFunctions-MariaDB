e018ce0ebf8f26d9298a3e08759b382f2f9d1a80
osawa-koki <github@osawa.dev>
2023-02-25 00:32:32 +0900

    UserPost

--- func/User.cs
+++ func/User.cs
@@ -6,14 +6,16 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using MySql.Data.MySqlClient;
 using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace my_namespace
 {
   public static partial class Program
   {
-    [FunctionName("User")]
+    [FunctionName("UserGet")]
     public static IActionResult UserGet(
-      [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+      [HttpTrigger(AuthorizationLevel.Function, "get", Route = "user")] HttpRequest req,
       ILogger log
     )
     {
@@ -33,6 +35,46 @@ namespace my_namespace
         }
 
         return new OkObjectResult(response);
+      }
+      catch (Exception ex)
+      {
+        return new BadRequestObjectResult(ex);
+      }
+    }
+
+    [FunctionName("UserPost")]
+    public static IActionResult UserPost(
+      [HttpTrigger(AuthorizationLevel.Function, "post", Route = "user")] HttpRequest req,
+      ILogger log
+    )
+    {
+      try
+      {
+        // JSONボディからデータを取得
+        string requestBody = new StreamReader(req.Body).ReadToEnd();
+        dynamic data = JsonConvert.DeserializeObject(requestBody);
+        string name = data?.name;
+
+        if (name == null)
+        {
+          return new BadRequestObjectResult("name is required");
+        }
+
+        MySqlConnection conn = new(Config.my_connection_string);
+        conn.Open();
+
+        string sql = "INSERT INTO users(name) VALUES(@user);";
+        MySqlCommand cmd = new(sql, conn);
+        cmd.Parameters.AddWithValue("@user", name);
+
+        // 作成したデータを取得
+        cmd.ExecuteNonQuery();
+        int id = (int)cmd.LastInsertedId;
+
+        return new OkObjectResult(new {
+          id = id,
+          name = name,
+        });
       }
       catch (Exception ex)
       {
